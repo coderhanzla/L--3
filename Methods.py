7c3ac8eec106c7af2032e5dee35ba7944dd4cdb6
coderhanzla@gmail.com <coderhanzla@gmail.com>
2024-11-30 19:40:47 +0530

    Methods in Opps

---
+++ Methods.py
@@ -0,0 +1,14 @@
+class student:
+    name = "Hanzla"
+    grade = 6
+
+    def introduction(self):
+        print("Hi i am a student")
+
+    def details(self):
+        print("My name is" , self.name)
+        print("My gade is" , self.grade)
+
+a= student()
+a.introduction()        
+a.details()        
